9beece9e314875693b4862dfaff7b16b60b11dda
4bhishekKasam <konnurabhishek@gmail.com>
2018-08-31 22:44:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      sort_term: '', movies: data.movies
+      sort_term: '', movies: data.movies, modMovies : data.movies,
     };
     this.onSorting = this.onSorting.bind(this);
+    this.searchMovie = this.searchMovie.bind(this);
   }
   onSorting = (e) => {
     let term = 'Title';
@@ -19,22 +20,46 @@ class App extends Component {
       return (option === 'asc' ? (a[term] >= b[term] ? 1 : -1) :
         (a[term] <= b[term] ? 1 : -1))
     });
-    this.setState({ sort_term: term });
-    this.setState({ movies: sortedList });
+    this.setState({ sort_term: term , modMovies: sortedList });
+  }
+
+  searchMovie(e){
+    let key = e;
+    console.log(e);
+    const { movies } = this.state;
+    if(!key) {
+      let orgList = movies.map(movie => { return movie });
+      this.setState({ modMovies : orgList });
+    } 
+    else {
+      let list  = movies.filter(movie => { 
+        return movie.Title.toLowerCase().includes(key.toLowerCase()); 
+      });
+      this.setState({ modMovies : list });
+    }
   }
 
   render() {
-    //   const { movies } = this.state;
+   const { modMovies } = this.state;
+   if (!modMovies.length){
+     return (
+      <div className="App">
+        <Header sort_term={this.state.sort_term}
+           onSorting={this.onSorting} 
+           searchHandler={this.searchMovie}  />  <br/>
+           <h4>No Data to display</h4>
+        </div>
+     )
+   }
     return (
       <div className="App">
         <Header sort_term={this.state.sort_term}
-          onSorting={this.onSorting} />
-
-        <Table movies={this.state.movies} />
-
+          onSorting={this.onSorting} 
+          searchHandler={this.searchMovie}  />
+        <Table movies={this.state.modMovies} />
       </div>
     );
-  }
+  } 
 }
 
 export default App;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import './Header.css';
+import React, { Component } from "react";
+import "./Header.css";
 
 class Header extends Component {
-    render() {
-        const {  onSorting } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = { searchKey: "" };
+    this.onSearch = this.onSearch.bind(this);
+  }
 
-        return (
-            <div className="nav">
-                <ul className="navLeft">
-                    <li >
-                        <form >
-                            <select onChange={onSorting}
-                                className="searchBar">
+  onSearch(e) {
+    e.preventDefault();
+    this.setState({ searchKey: e.target.value });
+  //  console.log(e.target.value);
+    this.props.searchHandler(e.target.value);
+  }
 
-                                <option value="asc"> Sort (A - Z)</option>
-                                <option value="desc"> Sort (Z - A) </option>
+  render() {
+    const { onSorting } = this.props;
 
-                            </select>
-                        </form>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div className="nav">
+        <ul className="navLeft">
+          <li>
+            <form>
+              <select onChange={onSorting} className="filterBox">
+                <option value="asc"> Sort (A - Z)</option>
+                <option value="desc"> Sort (Z - A) </option>
+              </select>
+            </form>
+          </li>
+          <li>
+            <form>
+              <input
+                type="search"
+                placeholder="Search by..."
+                className="searchBar"
+                onChange={this.onSearch}
+                value={this.state.searchKey}
+              />
+            </form>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Header;
